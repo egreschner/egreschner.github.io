466dab34593f7f84095485ef5b96896998311f6e
egreschner <e.greschner@gmail.com>
2016-02-09 21:30:46 -0500

    Php

---
+++ shanti/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html> 
+	<head>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>
+		Shanti
+		</title>
+	</head>
+	<body>
+		<div class="wrapper">
+		<?php include ("includes/header.html");?>
+		<section class="clearfix">
+			<article id="left" class="about"><!--Blog Post Sections-->
+					<p id="one">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+					</p>
+					<p id="two">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+			</article>
+        	<aside id="right" class="picture"><!--About me Section-->
+			A Little More About Me
+			<p class="about">
+			I am an aspiring Ashtangi student. At the moment I am trying to get back up with the physical limb of yoga and trying to improve on the other seven limbs. This blog travels with me as I learn and improve each limb of Ashtanga Yoga.
+			</p>
+			</aside>
+		</section>
+		<footer><!--Reiterate how to contact, navigation-->
+			<p class="lotion"><!--Questions & Comments Section-->
+			Questions or ideas? Email me at: <a href="mailto:liz@lizgres.com" q> liz@lizgres.com</a>
+			</p>	
+		</footer>
+		</div>
+	</body>
